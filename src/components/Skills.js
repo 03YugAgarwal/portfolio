706e7219ad21506b949654a31e8b2bed635eedee
Yug <03yugagarwal@gmail.com>
2023-07-05 18:36:35 +0530

    responsive

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,20 +15,31 @@ import {
 import { useMediaQuery } from "react-responsive";
 
 const Skills = () => {
-  const phoneViewNav = {
-    size: "2.5em",
-  };
-  const DesktopViewNav = {
-    size: "3.5em",
-  };
-  const isDesktopOrLaptop = useMediaQuery({maxWidth:830})
-    const iconSize = isDesktopOrLaptop ? phoneViewNav : DesktopViewNav
+
+  // const phoneViewNav = {
+  //   size: "2.5em",
+  // };
+  // const DesktopViewNav = {
+  //   size: "3.5em",
+  // };
+  // const isDesktopOrLaptop = useMediaQuery({minWidth:900})
+  const isTablet = useMediaQuery({maxWidth:900,minWidth:600})
+  const isPhone = useMediaQuery({maxWidth:600})
+
+  let size = '3.5rem'
+  if(isTablet){
+    size='2.5rem'
+  }
+  if(isPhone){
+    size='1.5rem'
+  }
+    
   return (
     <div className={styles.container}>
       <h4 className={styles.header4}>Here are my skills:</h4>
       <IconContext.Provider
         value={{
-          size: iconSize.size,
+          size: size,
           color: "#2c2826",
         }}
       >
