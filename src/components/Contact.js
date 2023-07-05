aa94b4adedf073b242075c346178d7461f77f1aa
Yug <03yugagarwal@gmail.com>
2023-07-05 15:19:00 +0530

    firebase integration

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,8 @@ const Contact = () => {
     message: "",
   });
 
+  const [postCheck, setPostCheck] = useState(false);
+
   const handleNameChange = (e) => {
     setDetails({ ...details, name: e.target.value });
   };
@@ -25,46 +27,55 @@ const Contact = () => {
 
   const handleClick = async (e) => {
     e.preventDefault();
-    const detailCollection = collection(db, "message");
 
+    // if (!details.name || !details.email || !details.subject || !details.msg) {
+    //   alert("Empty");
+    //   return;
+    // }
+
+    if (postCheck) {
+      return;
+    }
+    const detailCollection = collection(db, "message");
+    setPostCheck(true);
     await addDoc(detailCollection, details);
 
     setDetails({
       name: "",
       email: "",
       subject: "",
-      message: "",
+      message: "Thank you! Your response has been submitted!",
     });
   };
 
   return (
     <div className={styles.container}>
       <form>
-      <h1 className={styles.contactHeader}>Get in touch with me</h1>
-          <label htmlFor="name" className={styles.label}>
-            Name*
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            required
-            onChange={handleNameChange}
-            value={details.name}
-            placeholder="Enter your name"
-          />
-          <label htmlFor="email" className={styles.label}>
-            Email*
-          </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            required
-            onChange={handleEmailChange}
-            value={details.email}
-            placeholder="Enter your email address"
-          />
+        <h1 className={styles.contactHeader}>Get in touch with me</h1>
+        <label htmlFor="name" className={styles.label}>
+          Name*
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          onChange={handleNameChange}
+          value={details.name}
+          placeholder="Enter your name"
+        />
+        <label htmlFor="email" className={styles.label}>
+          Email*
+        </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          required
+          onChange={handleEmailChange}
+          value={details.email}
+          placeholder="Enter your email address"
+        />
 
         <label htmlFor="subjetc" className={styles.label}>
           Subject*
@@ -77,12 +88,18 @@ const Contact = () => {
           onChange={handleSubjectChange}
           value={details.subject}
           placeholder="Enter your subject"
-          />
+        />
         <label htmlFor="message" className={styles.label}>
           Message
         </label>
-        <textarea type="text" onChange={handleMsgChange} value={details.message}  className={styles.msg} />
-         
+        <textarea
+          type="text"
+          onChange={handleMsgChange}
+          value={details.message}
+          className={`${!postCheck ? styles.msg : styles.msgActive}`}
+          readOnly={postCheck}
+        />
+
         <button onClick={handleClick} className={styles.btn}>
           Send
         </button>
